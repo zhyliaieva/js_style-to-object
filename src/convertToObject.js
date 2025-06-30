6677cbd556759a84dada0f3471731893e594d090
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2025-06-30 13:37:51 +0300

    Convert Style String to Object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,16 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const arrayStrings = sourceString.split(';');
+  const arrayWithNoSpaces = arrayStrings.map((element) => element.trim());
+
+  const result = arrayWithNoSpaces.reduce((status, element, index) => {
+    const parts = element.split(':').map((part) => part.trim());
+
+    return { ...status, [parts[0]]: parts[1] };
+  }, {});
+
+  return result;
 }
 
 module.exports = convertToObject;
